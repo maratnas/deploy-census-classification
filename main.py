491e0c219f62e638ee0d9afd0ba3f73d4bb9f030
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2022-10-31 14:32:39 -0700

    Make app use ML model to infer salary classes.

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ Salary classification app.
 """
 import os
 import pickle
-from typing import Optional, Union
+from typing import Any, Dict, Optional, Union
 
+import numpy as np
 import pandas as pd
 from fastapi import FastAPI
 from sklearn.ensemble import RandomForestClassifier
 
 from census_features import CensusFeatures
 from ml.data import process_data_for_inference
-from ml.model import AugmentedModel, infer
+from ml.model import AugmentedModel, infer as infer_
 from train_model import LABEL, CATEGORICAL_FEATURES, MODEL_FILE_PATH
 
 
@@ -32,12 +33,12 @@ app = FastAPI(
 )
 
 
-augmented_model: Optional[AugmentedModel] = None
-@app.on_event("startup")
+startup_items: Dict[str, Any] = {}
+@app.on_event("startup") # => no responses until model has finished loading.
 async def load_model():
-    """Load model on app startup, not every query."""
+    """Load model once on app startup, not every query."""
     with open(MODEL_FILE_PATH, 'rb') as fin:
-        augmented_model = pickle.load(fin)
+        startup_items["augmented_model"] = pickle.load(fin)
 
 
 @app.get("/")
@@ -56,15 +57,26 @@ async def infer(features: CensusFeatures):
     Returns:
         Salary classification in {"<=50k", ">50k"}.
     """
+    augmented_model: AugmentedModel = startup_items["augmented_model"]
     data_frame = pd.DataFrame([features.dict()])
     data_frame.rename(
         lambda feature_name: feature_name.replace("_", "-"),
         axis="columns",
         inplace=True,
     )
+    X: np.ndarray
+    X, _ = process_data_for_inference(
+        data_frame=data_frame,
+        categorical_features=CATEGORICAL_FEATURES,
+        input_encoder=augmented_model.input_encoder,
+        label_binarizer=augmented_model.label_binarizer,
+    )
 
-    # TODO: Finish this.
+    # `inferred_class`` takes a value in {"<=50k", ">50k"}.
+    y_inferred = infer_(augmented_model.model, X)
+    inferred_class: str = augmented_model.label_binarizer.inverse_transform(
+        y_inferred,
+    )[0]
 
-    #salary_class: str = "<=50k"
-    salary_class: str = ">50k"
-    return {"inferred_salary_class": f"{salary_class}"}
+    #inferred_class: str = "<=50k"  # Place-holder for testing only.
+    return {"inferred_salary_class": f"{inferred_class}"}
