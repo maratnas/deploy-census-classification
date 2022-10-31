a6dd535acbacd0b2ac6fa5310f960448c2b77f2b
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2022-10-31 14:59:37 -0700

    Add examples to test both possible PUT responses of API.

--- test_api.py
+++ test_api.py
@@ -12,6 +12,7 @@ $ ./test_api.py  --local  # Test API locally.
 """
 import argparse
 import json
+from typing import Dict, Any
 
 import requests
 
@@ -30,23 +31,39 @@ else:
     base_url = "https://census-classification-a4a401cd.herokuapp.com/"
 
 # Load example.
-data = CensusFeatures.Config.schema_extra["example"]
-# data = {
-#     "age": 40,
-#     "workclass": "Self-emp-not-inc",
-#     "fnlgt": 338409,
-#     "education": "Masters",
-#     "education_num": 14,
-#     "marital_status": "Never-married",
-#     "occupation": "Exec-managerial",
-#     "relationship": "Not-in-family",
-#     "race": "White",
-#     "sex": "Female",
-#     "capital_gain": 2174,
-#     "capital_loss": 0,
-#     "hours_per_week": 40,
-#     "native_country": "United-States",
-# }
+#data = CensusFeatures.Config.schema_extra["example"]
+example_low_salary = {
+    "age": 16,
+    "workclass": "Never-worked",
+    "fnlgt": 77516,
+    "education": "Preschool",
+    "education_num": 1,
+    "marital_status": "Never-married",
+    "occupation": "Other-service",
+    "relationship": "Unmarried",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 2174,
+    "capital_loss": 1411,
+    "hours_per_week": 5,
+    "native_country": "United-States",
+}
+example_high_salary = {
+    "age": 40,
+    "workclass": "Self-emp-not-inc",
+    "fnlgt": 338409,
+    "education": "Doctorate",
+    "education_num": 16,
+    "marital_status": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Wife",
+    "race": "Black",
+    "sex": "Female",
+    "capital_gain": 5060,
+    "capital_loss": 0,
+    "hours_per_week": 40,
+    "native_country": "United-States",
+}
 
 
 def test_get():
@@ -60,11 +77,10 @@ def test_get():
     )
 
 
-def test_post():
+def test_post(example: Dict[str, Any]):
     """Confirm inferred classes are correctly returned."""
-    # TODO: Add cases for both model output classes.
     url = base_url + "salary_class_inference/"
-    response = requests.post(url, data=json.dumps(data))
+    response = requests.post(url, data=json.dumps(example))
     print(
         "PUT\n"
         f"response: {response.json()}\n"
@@ -73,15 +89,15 @@ def test_post():
 
 
 def main():
-    """Run tests."""
+    """Run tests and print results."""
     print()
     test_get()
 
     print()
-    test_post()
+    test_post(example_low_salary)
 
     print()
-    test_post()
+    test_post(example_high_salary)
 
     print()
 
